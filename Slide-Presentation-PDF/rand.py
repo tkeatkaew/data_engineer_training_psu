2d3521990b8443a8e4b32fadffae91d2e155993f
tkeatkaew <thanit@rmutl.ac.th>
2025-06-18 20:33:32 +0700

    remove some files

--- Slide-Presentation-PDF/rand.py
+++
@@ -1,7 +0,0 @@
-import random
-from datetime import datetime
-now = datetime.now()
-num = random.randint(1, 101)
-with open('/tmp/rand.txt', 'a') as f:
-	f.write('{} - Your random number is {}\n'.format(now, num))
-
